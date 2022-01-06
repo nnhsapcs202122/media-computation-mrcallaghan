76d879e9d67e74c1a4835e118d454a678fd7dcfd
Matthew Callaghan <mcallaghan@Naperville203.org>
2022-01-06 10:56:35 -0600

    Finished first method in ArrayList notes

--- ArrayListNotes.java
+++ ArrayListNotes.java
@@ -3,14 +3,27 @@ import java.util.ArrayList;
 /**
  * Write a description of class ArrayListNotes here.
  *
- * @author gcschmit
+ * @author mrcallaghan
  * @version 
  */
 public class ArrayListNotes
 {
     public static void main(String[] args)
     {
-        
+        /*
+         * The ArrayList is a Java Generic.
+         * 
+         * We have to specify the type of elements in the list in angle brackets.
+         *      (i.e. < >) after every ArrayList identifier.
+         *      
+         * Primitives (e.g., int, double) are not classes and cannot be specified as the type of the 
+         *      elements in a Generic.  Instead, we use the corresponding wrapper classes (e.g. Integer,
+         *      Double, Boolean).
+         */
+        ;
+        ArrayList<Integer> myList;
+        myList = createRandomIntegerList(10,20);
+        System.out.println(myList);
     }
     
     /**
@@ -25,9 +38,26 @@ public class ArrayListNotes
      */
     public static ArrayList<Integer> createRandomIntegerList(int size, int range)
     {
-        return null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        for(int i = 0; i < size; i++)
+        {
+            int value = (int)(Math.random() * range) + 1;
+            
+            /*
+             * The add method adds the specified object to the end of the list.
+             * 
+             * Autoboxing: 
+             *  Primitive values are automatically converted to their corresponding wrapper class.
+             *  However, type promotion does not occur.
+             */
+            
+            list.add(value);
+        }
+        
+        return list;
     }
-    	 
+         
     
     /**
      * Removes even numbers from the specified list.
@@ -36,7 +66,7 @@ public class ArrayListNotes
      */
     public static void removeEvens(ArrayList<Integer> list)
     {
-    	
+        
     }
     
     public static void removeEvensAlt(ArrayList<Integer> list)
